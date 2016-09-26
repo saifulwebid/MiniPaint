384c0e92a42a5e83af6475812b3ef2964f80ae08
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-09-26 11:10:09 +0700

    Draw canvas from stack of lines

--- MiniPaint.WinForms/frmMain.cs
+++ MiniPaint.WinForms/frmMain.cs
@@ -12,30 +12,26 @@ namespace MiniPaint.WinForms
 {
     public partial class frmMain : Form
     {
+        private Stack<DrawingObject.Line> lines;
+
         public frmMain()
         {
             InitializeComponent();
+
+            lines = new Stack<DrawingObject.Line>();
+            lines.Push(new DrawingObject.Line(new Point(10, 10), new Point(100, 200)));
+            lines.Push(new DrawingObject.Line(new Point(30, 10), new Point(100, 100)));
+            lines.Push(new DrawingObject.Line(new Point(20, 100), new Point(21, 101)));
+            lines.Push(new DrawingObject.Line(new Point(10, 100), new Point(20, 10)));
         }
 
         private void pnlCanvas_Paint(object sender, PaintEventArgs e)
         {
-            DrawingObject.Line l;
-
-            l = new DrawingObject.Line(new Point(10, 10), new Point(100, 200));
-            l.DrawDDA(e.Graphics);
-            l.DrawNaive(e.Graphics);
-
-            l = new DrawingObject.Line(new Point(30, 10), new Point(100, 100));
-            l.DrawDDA(e.Graphics);
-            l.DrawNaive(e.Graphics);
-
-            l = new DrawingObject.Line(new Point(20, 100), new Point(21, 101));
-            l.DrawDDA(e.Graphics);
-            l.DrawNaive(e.Graphics);
-
-            l = new DrawingObject.Line(new Point(10, 100), new Point(20, 10));
-            l.DrawDDA(e.Graphics);
-            l.DrawNaive(e.Graphics);
+            foreach (DrawingObject.Line l in lines)
+            {
+                l.DrawDDA(e.Graphics);
+                l.DrawNaive(e.Graphics);
+            }
         }
     }
 }
