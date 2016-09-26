8d865a3f5983c26323a4ce2d074ac74707650a1f
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-09-26 13:14:00 +0700

    Modify AssemblyInfo

--- MiniPaint.WinForms/Properties/AssemblyInfo.cs
+++ MiniPaint.WinForms/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiniPaint.WinForms")]
+[assembly: AssemblyTitle("MiniPaint")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiniPaint.WinForms")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("MiniPaint")]
+[assembly: AssemblyCopyright("Copyright © Muhammad Saiful Islam, 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
