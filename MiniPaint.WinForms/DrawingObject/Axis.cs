ec3b7cd947f896f6bb3e28f864a2f0fcea8837cc
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-11-06 14:33:50 +0700

    Add PolynomialFunction and Axis drawing object

---
+++ MiniPaint.WinForms/DrawingObject/Axis.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPaint.WinForms.DrawingObject
+{
+    class Axis : IDrawable
+    {
+        public int Height, Width;
+        public int Scale;
+
+        public Axis(int height, int width, int scale)
+        {
+            Height = height;
+            Width = width;
+            Scale = scale;
+        }
+
+        public void ResetAxisSize(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+        
+        public void Draw(Graphics g)
+        {
+            Point center = new Point(Width / 2, Height / 2);
+
+            new LineGenerator.Bresenham(new Line(new Point(0, center.Y), new Point(Width, center.Y))).Draw(g);
+            new LineGenerator.Bresenham(new Line(new Point(center.X, 0), new Point(center.X, Height))).Draw(g);
+
+            int x, y;
+            for (x = center.X + Scale; x <= Width; x += Scale)
+            {
+                new LineGenerator.Bresenham(new Line(new Point(x, center.Y - 5), new Point(x, center.Y + 5))).Draw(g);
+            }
+            for (x = center.X - Scale; x >= 0; x -= Scale)
+            {
+                new LineGenerator.Bresenham(new Line(new Point(x, center.Y - 5), new Point(x, center.Y + 5))).Draw(g);
+            }
+            for (y = center.Y + Scale; y <= Height; y += Scale)
+            {
+                new LineGenerator.Bresenham(new Line(new Point(center.X - 5, y), new Point(center.X + 5, y))).Draw(g);
+            }
+            for (y = center.Y - Scale; y >= 0; y -= Scale)
+            {
+                new LineGenerator.Bresenham(new Line(new Point(center.X - 5, y), new Point(center.X + 5, y))).Draw(g);
+            }
+        }
+    }
+}
---
+++ MiniPaint.WinForms/DrawingObject/Function.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPaint.WinForms.DrawingObject
+{
+    class PolynomialFunction : IDrawable
+    {
+        private double[] constants;
+        public int Height, Width;
+        public int Scale;
+
+        public PolynomialFunction(double[] constants, int height, int width, int scale)
+        {
+            this.constants = constants;
+            Height = height;
+            Width = width;
+            Scale = scale;
+        }
+
+        public void ResetAxisSize(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        private double yResult(double x)
+        {
+            double result = 0;
+            for (int i = 0; i < constants.Length; i++)
+            {
+                double xPower = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    xPower = xPower * x;
+                }
+                result += constants[i] * xPower;
+            }
+            return result;
+        }
+
+        private int yPlotter(double x)
+        {
+            int center = Height / 2;
+
+            return center - (int)(yResult(x) * Scale);
+        }
+
+        public void Draw(Graphics g)
+        {
+            Point center = new Point(Width / 2, Height / 2);
+
+            int x_prev = 0;
+            int y_prev = yPlotter(x_prev);
+
+            for (int x = 1; x <= Width - center.X; x++)
+            {
+                int y = yPlotter(x);
+
+                if (y_prev >= 0 && y_prev <= Height)
+                    new LineGenerator.Dda(new Line(new Point(x_prev + center.X, y_prev), new Point(x + center.X, y))).Draw(g);
+
+                x_prev = x;
+                y_prev = y;
+            }
+
+            x_prev = 0;
+            y_prev = yPlotter(x_prev);
+
+            for (int x = -1; x >= -center.X; x--)
+            {
+                int y = yPlotter(x);
+
+                if (y_prev >= 0 && y_prev <= Height)
+                    new LineGenerator.Dda(new Line(new Point(x_prev + center.X, y_prev), new Point(x + center.X, y))).Draw(g);
+
+                x_prev = x;
+                y_prev = y;
+            }
+        }
+    }
+}
