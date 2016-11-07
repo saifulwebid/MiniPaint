5f04f060e91b1dc236803eb426ce726bf8a5cdf5
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-11-07 14:09:57 +0700

    Change "edges" to "n" label

--- MiniPaint.WinForms/frmMain.Designer.cs
+++ MiniPaint.WinForms/frmMain.Designer.cs
@@ -259,7 +259,7 @@
             // 
             // txtNGonEdges
             // 
-            this.txtNGonEdges.Location = new System.Drawing.Point(71, 28);
+            this.txtNGonEdges.Location = new System.Drawing.Point(38, 28);
             this.txtNGonEdges.Name = "txtNGonEdges";
             this.txtNGonEdges.Size = new System.Drawing.Size(45, 22);
             this.txtNGonEdges.TabIndex = 2;
@@ -280,9 +280,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.Size = new System.Drawing.Size(20, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Edges:";
+            this.label1.Text = "n:";
             // 
             // pbxCanvas
             // 
