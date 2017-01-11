414e4946d9fcaac08d0d4436984064005728de1d
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 14:16:46 +0700

    Allow scaling to be based other than (0, 0)

--- MiniPaint.WinForms/Transformation/ScalingMatrixFactory.cs
+++ MiniPaint.WinForms/Transformation/ScalingMatrixFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,15 @@ namespace MiniPaint.WinForms.Transformation
         public static Matrix CreateMatrix(double sc)
         {
             return new Matrix(sc, 0, 0, sc, 0, 0);
+        }
+
+        public static Matrix CreateMatrix(double sc, Point p)
+        {
+            Matrix m = TranslationMatrixFactory.CreateMatrix(-p.X, -p.Y);
+            m.Apply(CreateMatrix(sc));
+            m.Apply(TranslationMatrixFactory.CreateMatrix(p.X, p.Y));
+
+            return m;
         }
     }
 }
