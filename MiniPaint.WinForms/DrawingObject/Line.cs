593ed9641d9f3e28f84c6619af800b564d062e80
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 19:53:59 +0700

    Make Line comply with IDrawable

--- MiniPaint.WinForms/DrawingObject/Line.cs
+++ MiniPaint.WinForms/DrawingObject/Line.cs
@@ -4,21 +4,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MiniPaint.WinForms.LineGenerator;
 
 namespace MiniPaint.WinForms.DrawingObject
 {
-    class Line
+    class Line : IDrawable
     {
-        public Point Start
-        {
-            get;
-        }
-
-        public Point End
-        {
-            get;
-        }
-
+        public Point Start { get; }
+        public Point End { get; }
         public double M
         {
             get
@@ -26,7 +19,6 @@ namespace MiniPaint.WinForms.DrawingObject
                 return (End.Y - Start.Y) / (End.X - Start.X);
             }
         }
-
         public double C
         {
             get
@@ -41,11 +33,19 @@ namespace MiniPaint.WinForms.DrawingObject
                 }
             }
         }
+        public Color ForegroundColor { get; set; }
+        public ILineGenerator LineGenerator { get; set; }
 
-        public Line(Point start, Point end)
+        public Line(Point start, Point end, ILineGenerator lg)
         {
             Start = start;
             End = end;
+            LineGenerator = lg;
+        }
+
+        public void Draw(Graphics g)
+        {
+            LineGenerator.Draw(g, this);
         }
     }
 }
