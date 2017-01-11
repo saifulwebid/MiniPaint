d603f0893da69efcb6011c958768db050861fb10
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 10:55:27 +0700

    Create RotationMatrixFactory

---
+++ MiniPaint.WinForms/Transformation/RotationMatrixFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPaint.WinForms.Transformation
+{
+    static class RotationMatrixFactory
+    {
+        public static Matrix CreateMatrix(Double d)
+        {
+            return new Matrix(Math.Cos(d), -Math.Sin(d), Math.Sin(d), Math.Cos(d), 0, 0);
+        }
+    }
+}
