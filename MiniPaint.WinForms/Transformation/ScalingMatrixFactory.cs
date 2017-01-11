3f39ac8b0efe3348a0c4298d060c41ede781f243
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 11:46:07 +0700

    Create ScalingMatrixFactory

---
+++ MiniPaint.WinForms/Transformation/ScalingMatrixFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPaint.WinForms.Transformation
+{
+    static class ScalingMatrixFactory
+    {
+        public static Matrix CreateMatrix(double sc)
+        {
+            return new Matrix(sc, 0, 0, sc, 0, 0);
+        }
+    }
+}
