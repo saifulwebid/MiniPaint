214017b2d3c2bff6b5bd568407ed64162aafbb65
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 19:57:27 +0700

    Make Bresenham comply with ILineGenerator

--- MiniPaint.WinForms/LineGenerator/Bresenham.cs
+++ MiniPaint.WinForms/LineGenerator/Bresenham.cs
@@ -4,42 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MiniPaint.WinForms.DrawingObject;
 
 namespace MiniPaint.WinForms.LineGenerator
 {
-    class Bresenham : IDrawable
+    class Bresenham : ILineGenerator
     {
-        private DrawingObject.Line line;
-
-        public Bresenham(DrawingObject.Line line)
-        {
-            this.line = line;
-        }
-
-        /// <summary>
+        /// Implementation taken from
         /// https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C
-        /// </summary>
-        public void Draw(Graphics g, Color c)
+        public void Draw(Graphics g, Line l)
         {
-            Brush br = new SolidBrush(c);
+            Brush br = new SolidBrush(l.ForegroundColor);
             Size sz = new Size(1, 1);
 
-            int dx = Math.Abs(line.End.X - line.Start.X);
-            int sx = line.Start.X < line.End.X ? 1 : -1;
+            int dx = Math.Abs(l.End.X - l.Start.X);
+            int sx = l.Start.X < l.End.X ? 1 : -1;
 
-            int dy = Math.Abs(line.End.Y - line.Start.Y);
-            int sy = line.Start.Y < line.End.Y ? 1 : -1;
+            int dy = Math.Abs(l.End.Y - l.Start.Y);
+            int sy = l.Start.Y < l.End.Y ? 1 : -1;
 
             int err = (dx > dy ? dx : -dy) / 2;
             int e2;
 
-            int x = line.Start.X;
-            int y = line.Start.Y;
+            int x = l.Start.X;
+            int y = l.Start.Y;
             while (true)
             {
                 g.FillRectangle(br, new Rectangle(new Point(x, y), sz));
 
-                if (x == line.End.X && y == line.End.Y)
+                if (x == l.End.X && y == l.End.Y)
                     break;
 
                 e2 = err;
