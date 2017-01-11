f109907dc1a445c0d0cbd59112a58a9d38fcc41a
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 20:05:20 +0700

    Make Axis comply with IDrawable

--- MiniPaint.WinForms/DrawingObject/Axis.cs
+++ MiniPaint.WinForms/DrawingObject/Axis.cs
@@ -4,19 +4,23 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MiniPaint.WinForms.LineGenerator;
 
 namespace MiniPaint.WinForms.DrawingObject
 {
     class Axis : IDrawable
     {
-        public int Height, Width;
-        public int Scale;
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Scale { get; set; }
+        public Color ForegroundColor { get; set; }
 
-        public Axis(int height, int width, int scale)
+        public Axis(int height, int width, int scale, Color fg)
         {
             Height = height;
             Width = width;
             Scale = scale;
+            ForegroundColor = fg;
         }
 
         public void ResetAxisSize(int height, int width)
@@ -25,29 +29,29 @@ namespace MiniPaint.WinForms.DrawingObject
             Width = width;
         }
         
-        public void Draw(Graphics g, Color c)
+        public void Draw(Graphics g)
         {
             Point center = new Point(Width / 2, Height / 2);
 
-            new LineGenerator.Bresenham(new Line(new Point(0, center.Y), new Point(Width, center.Y))).Draw(g, c);
-            new LineGenerator.Bresenham(new Line(new Point(center.X, 0), new Point(center.X, Height))).Draw(g, c);
+            new Line(new Point(0, center.Y), new Point(Width, center.Y), ForegroundColor, new Bresenham()).Draw(g);
+            new Line(new Point(center.X, 0), new Point(center.X, Height), ForegroundColor, new Bresenham()).Draw(g);
 
             int x, y;
             for (x = center.X + Scale; x <= Width; x += Scale)
             {
-                new LineGenerator.Bresenham(new Line(new Point(x, center.Y - 5), new Point(x, center.Y + 5))).Draw(g, c);
+                new Line(new Point(x, center.Y - 5), new Point(x, center.Y + 5), ForegroundColor, new Bresenham()).Draw(g);
             }
             for (x = center.X - Scale; x >= 0; x -= Scale)
             {
-                new LineGenerator.Bresenham(new Line(new Point(x, center.Y - 5), new Point(x, center.Y + 5))).Draw(g, c);
+                new Line(new Point(x, center.Y - 5), new Point(x, center.Y + 5), ForegroundColor, new Bresenham()).Draw(g);
             }
             for (y = center.Y + Scale; y <= Height; y += Scale)
             {
-                new LineGenerator.Bresenham(new Line(new Point(center.X - 5, y), new Point(center.X + 5, y))).Draw(g, c);
+                new Line(new Point(center.X - 5, y), new Point(center.X + 5, y), ForegroundColor, new Bresenham()).Draw(g);
             }
             for (y = center.Y - Scale; y >= 0; y -= Scale)
             {
-                new LineGenerator.Bresenham(new Line(new Point(center.X - 5, y), new Point(center.X + 5, y))).Draw(g, c);
+                new Line(new Point(center.X - 5, y), new Point(center.X + 5, y), ForegroundColor, new Bresenham()).Draw(g);
             }
         }
     }
