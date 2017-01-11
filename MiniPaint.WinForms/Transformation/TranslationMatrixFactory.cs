bc55abdf127e045a98ebf495522b070bf2f65b23
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 10:56:51 +0700

    Create TranslationMatrixFactory

---
+++ MiniPaint.WinForms/Transformation/TranslationMatrixFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPaint.WinForms.Transformation
+{
+    static class TranslationMatrixFactory
+    {
+        public static Matrix CreateMatrix(int dx, int dy)
+        {
+            return new Matrix(1, 0, 0, 1, dx, dy);
+        }
+    }
+}
