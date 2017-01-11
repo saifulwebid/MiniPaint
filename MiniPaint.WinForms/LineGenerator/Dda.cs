dd3f42df93b9a2cd4139ba4d3431b7a5069daf6f
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 19:58:38 +0700

    Make Dda comply with ILineGenerator

--- MiniPaint.WinForms/LineGenerator/Dda.cs
+++ MiniPaint.WinForms/LineGenerator/Dda.cs
@@ -4,32 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MiniPaint.WinForms.DrawingObject;
 
 namespace MiniPaint.WinForms.LineGenerator
 {
-    class Dda : IDrawable
+    class Dda : ILineGenerator
     {
-        private DrawingObject.Line line;
-
-        public Dda(DrawingObject.Line line)
+        public void Draw(Graphics g, Line l)
         {
-            this.line = line;
-        }
-
-        public void Draw(Graphics g, Color c)
-        {
-            Brush br = new SolidBrush(c);
+            Brush br = new SolidBrush(l.ForegroundColor);
             Size sz = new Size(1, 1);
 
-            int dx = line.End.X - line.Start.X;
-            int dy = line.End.Y - line.Start.Y;
+            int dx = l.End.X - l.Start.X;
+            int dy = l.End.Y - l.Start.Y;
 
             int step = Math.Max(Math.Abs(dx), Math.Abs(dy));
             double x_tambah = (double)dx / step;
             double y_tambah = (double)dy / step;
 
-            double x = line.Start.X;
-            double y = line.Start.Y;
+            double x = l.Start.X;
+            double y = l.Start.Y;
             while (step >= 0)
             {
                 g.FillRectangle(br, new Rectangle(new Point((int)x, (int)y), sz));
