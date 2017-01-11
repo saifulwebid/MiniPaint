f462d65fffa6acbe56851452c175ae8e491117c0
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-11 19:03:46 +0700

    Define ILineGenerator contract Preparation to use Strategy design pattern

---
+++ MiniPaint.WinForms/LineGenerator/ILineGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniPaint.WinForms.DrawingObject;
+
+namespace MiniPaint.WinForms.LineGenerator
+{
+    interface ILineGenerator
+    {
+        void Draw(Graphics g, Line l);
+    }
+}
