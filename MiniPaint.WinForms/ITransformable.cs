97af4dc58cbe21de8990c12eee7dcd64ae315e20
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-15 21:50:20 +0700

    Create ITransformable interface

---
+++ MiniPaint.WinForms/ITransformable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniPaint.WinForms.Transformation;
+
+namespace MiniPaint.WinForms
+{
+    interface ITransformable
+    {
+        Matrix TransformationMatrix { get; }
+    }
+}
