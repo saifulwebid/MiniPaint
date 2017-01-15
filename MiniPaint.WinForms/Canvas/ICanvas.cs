50c0e92a9565b47bb450835b20b580090b9d3230
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-16 05:18:08 +0700

    Add Bitmap, Height, Width property to ICanvas

--- MiniPaint.WinForms/Canvas/ICanvas.cs
+++ MiniPaint.WinForms/Canvas/ICanvas.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace MiniPaint.WinForms.Canvas
 {
     interface ICanvas
     {
+        Bitmap Bitmap { get; set; }
+        int Height { get; set; }
+        int Width { get; set; }
     }
 }
--- MiniPaint.WinForms/Canvas/PolynomialFunctionCanvas.cs
+++ MiniPaint.WinForms/Canvas/PolynomialFunctionCanvas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,49 @@ namespace MiniPaint.WinForms.Canvas
 {
     class PolynomialFunctionCanvas : ICanvas
     {
+        private int height;
+        private int width;
+
+        public Bitmap Bitmap { get; set; }
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                height = value;
+                Resize();
+            }
+        }
+        public int Width
+        {
+            get { return width; }
+            set
+            {
+                width = value;
+                Resize();
+            }
+        }
+
+        public PolynomialFunctionCanvas(int height, int width)
+        {
+            Bitmap = new Bitmap(height, width);
+            this.height = height;
+            this.width = width;
+        }
+
+        private void Resize()
+        {
+            Bitmap newBitmap = new Bitmap(Height, Width);
+
+            if (Bitmap != null)
+            {
+                using (Graphics g = Graphics.FromImage(newBitmap))
+                {
+                    g.DrawImage(Bitmap, 0, 0);
+                }
+                Bitmap.Dispose();
+            }
+            Bitmap = newBitmap;
+        }
     }
 }
