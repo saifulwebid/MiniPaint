5a2d2d6858b9dc92e103767eb2bf8829c9a9cd90
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-15 18:57:48 +0700

    Create CreateDrawableObject action

---
+++ MiniPaint.WinForms/Action/CreateDrawableObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MiniPaint.WinForms.Action
+{
+    class CreateDrawableObject : IAction
+    {
+        private IDrawable drawableObject;
+
+        public CreateDrawableObject(IDrawable o)
+        {
+            drawableObject = o;
+        }
+
+        public void Do(Graphics g)
+        {
+            drawableObject.Draw(g);
+        }
+    }
+}
