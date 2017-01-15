84a18feeea1bd23ac434822354e0d1da07251fa2
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-15 21:38:51 +0700

    Create DrawingCanvas and PolynomialFunctionCanvas

---
+++ MiniPaint.WinForms/Canvas/DrawingCanvas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPaint.WinForms.Canvas
+{
+    class DrawingCanvas : ICanvas
+    {
+    }
+}
