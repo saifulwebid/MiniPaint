6939341dc7a1274f1f47593e0df08891f7eba9b1
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-16 09:01:16 +0700

    Create CreatePolynomialFunctionObject action

---
+++ MiniPaint.WinForms/Action/CreatePolynomialFunctionObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniPaint.WinForms.Canvas;
+using MiniPaint.WinForms.DrawingObject;
+
+namespace MiniPaint.WinForms.Action
+{
+    class CreatePolynomialFunctionObject : IAction
+    {
+        private PolynomialFunction drawableObject;
+        private PolynomialFunctionCanvas canvas;
+
+        public CreatePolynomialFunctionObject(PolynomialFunction o, PolynomialFunctionCanvas c)
+        {
+            drawableObject = o;
+            canvas = c;
+        }
+        public void Do()
+        {
+            canvas.Objects.Add(drawableObject);
+        }
+    }
+}
