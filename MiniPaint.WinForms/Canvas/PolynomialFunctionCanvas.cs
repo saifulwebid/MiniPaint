1f823a9ad954f7a3961d6f60f1836c69e0111917
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-16 09:06:45 +0700

    Create Objects, AxisColor, and Scale property in PolynomialFunctionCanvas

--- MiniPaint.WinForms/Canvas/PolynomialFunctionCanvas.cs
+++ MiniPaint.WinForms/Canvas/PolynomialFunctionCanvas.cs
@@ -4,12 +4,49 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using MiniPaint.WinForms.DrawingObject;
 
 namespace MiniPaint.WinForms.Canvas
 {
     class PolynomialFunctionCanvas : Canvas
     {
+        private Axis axis;
+        private Color axisColor;
+        private int scale;
+
+        public ObservableCollection<PolynomialFunction> Objects { get; set; }
+        public Color AxisColor
+        {
+            get { return axisColor; }
+            set
+            {
+                axisColor = value;
+                axis.ForegroundColor = value;
+            }
+        }
+        public int Scale
+        {
+            get { return scale; }
+            set
+            {
+                scale = value;
+                axis.Scale = value;
+            }
+        }
+
         public PolynomialFunctionCanvas(int width, int height, Color axisColor) : base(width, height)
+        {
+            Objects = new ObservableCollection<PolynomialFunction>();
+            axis = new Axis(width, height, (1 << 6), axisColor);
+
+            AxisColor = axis.ForegroundColor;
+            Scale = axis.Scale;
+
+            Objects.CollectionChanged += Objects_CollectionChanged;
+        }
+
+        private void Objects_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
         }
 
