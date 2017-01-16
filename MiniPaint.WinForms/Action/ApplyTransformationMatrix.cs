448446f5e47a0718f90127ba6164b090216684fa
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-16 09:50:33 +0700

    Add ApplyTransformationMatrix action

---
+++ MiniPaint.WinForms/Action/ApplyTransformationMatrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniPaint.WinForms.Transformation;
+
+namespace MiniPaint.WinForms.Action
+{
+    class ApplyTransformationMatrix : IAction
+    {
+        private ITransformable drawableObject;
+        private Matrix transformationMatrix;
+
+        public ApplyTransformationMatrix(ITransformable o, Matrix m)
+        {
+            drawableObject = o;
+            transformationMatrix = m;
+        }
+        
+        public void Do()
+        {
+            drawableObject.TransformationMatrix.Apply(transformationMatrix);
+        }
+    }
+}
