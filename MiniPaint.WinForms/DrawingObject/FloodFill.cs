70dad9c79b58b68d160a3e1b24ba7e705f8411db
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-16 13:32:00 +0700

    Implement FloodFill

---
+++ MiniPaint.WinForms/DrawingObject/FloodFill.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniPaint.WinForms.Canvas;
+
+namespace MiniPaint.WinForms.DrawingObject
+{
+    class FloodFill : IDrawable
+    {
+        private Point seedPoint;
+        private DrawingCanvas canvas;
+
+        public string DisplayText
+        {
+            get { return "Flood fill"; }
+        }
+
+        public Color ForegroundColor { get; set; }
+
+        public FloodFill(Point seedPoint, Color newColor, DrawingCanvas c)
+        {
+            this.seedPoint = seedPoint;
+            ForegroundColor = newColor;
+            canvas = c;
+        }
+
+        public void Draw(Graphics g)
+        {
+            /* Initialize put-pixel operation */
+            Brush br = new SolidBrush(ForegroundColor);
+            Size sz = new Size(1, 1);
+
+            /* Get old bitmap */
+            Bitmap bmp = canvas.Bitmap;
+            Color oldColor = bmp.GetPixel(seedPoint.X, seedPoint.Y);
+
+            Stack<Point> stack = new Stack<Point>();
+            stack.Push(seedPoint);
+            while (stack.Count > 0)
+            {
+                Point current = stack.Pop();
+                bool execute = true;
+
+                /* Base case */
+                if (current.X < 0 || current.X > canvas.Width ||
+                        current.Y < 0 || current.Y > canvas.Height)
+                    execute = false;
+
+                if (execute)
+                {
+                    Color currentColor = bmp.GetPixel(current.X, current.Y);
+                    if (currentColor.ToArgb() != oldColor.ToArgb())
+                        execute = false;
+
+                    if (execute)
+                    {
+                        /* Recurrence */
+                        bmp.SetPixel(current.X, current.Y, ForegroundColor);
+
+                        stack.Push(new Point(current.X + 1, current.Y));
+                        stack.Push(new Point(current.X - 1, current.Y));
+                        stack.Push(new Point(current.X, current.Y + 1));
+                        stack.Push(new Point(current.X, current.Y - 1));
+                    }
+                }
+            }
+        }
+    }
+}
