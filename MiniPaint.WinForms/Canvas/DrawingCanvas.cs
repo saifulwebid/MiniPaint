3e2f9567eb48f3940c70cd3c19c1e60f53b4ad4d
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-01-16 07:15:19 +0700

    Add "preview-object" ability to DrawingCanvas

--- MiniPaint.WinForms/Canvas/DrawingCanvas.cs
+++ MiniPaint.WinForms/Canvas/DrawingCanvas.cs
@@ -9,8 +9,37 @@ namespace MiniPaint.WinForms.Canvas
 {
     class DrawingCanvas : Canvas
     {
+        private Bitmap preDragBitmap;
+
         public DrawingCanvas(int height, int width) : base(height, width)
         {
+            preDragBitmap = null;
+        }
+
+        public void PrepareForDragging()
+        {
+            preDragBitmap = (Bitmap)Bitmap.Clone();
+        }
+
+        public void PreviewOnDrag(IDrawable o)
+        {
+            Bitmap.Dispose();
+            Bitmap = (Bitmap)preDragBitmap.Clone();
+            using (Graphics g = Graphics.FromImage(Bitmap))
+            {
+                o.Draw(g);
+            }
+
+            OnBitmapChanged();
+        }
+
+        public void FinishDragging()
+        {
+            Bitmap.Dispose();
+            Bitmap = (Bitmap)preDragBitmap.Clone();
+            preDragBitmap.Dispose();
+            preDragBitmap = null;
+        }
         }
     }
 }
